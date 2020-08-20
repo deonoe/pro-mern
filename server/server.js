b67f9398bac01bdc4c576305f942f9010e1b119e
deonoe <56742340+deonoe@users.noreply.github.com>
2020-08-20 15:49:17 +0200

    The Create API

--- server/server.js
+++ server/server.js
@@ -2,12 +2,21 @@ const fs = require("fs");
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const { GraphQLScalarType } = require("graphql");
+const { Kind } = require("graphql/language");
 
 const GraphQLDate = new GraphQLScalarType({
   name: "GraphQLDate",
   description: "A Date() type in GraphQL as a scalar",
   serialize(value) {
     return value.toISOString();
+  },
+
+  parseValue(value) {
+    return new Date(value);
+  },
+
+  parseLiteral(ast) {
+    return ast.kind === Kind.STRING ? new Date(ast.value) : undefined;
   },
 });
 
@@ -41,12 +50,21 @@ const resolvers = {
   },
   Mutation: {
     setAboutMessage,
+    issueAdd,
   },
   GraphQLDate,
 };
 
 function setAboutMessage(_, { message }) {
   return (aboutMessage = message);
+}
+
+function issueAdd(_, { issue }) {
+  issue.created = new Date();
+  issue.id = issuesDB.length + 1;
+  if (issue.status == undefined) issue.status = "New";
+  issuesDB.push(issue);
+  return issue;
 }
 
 function issueList() {
