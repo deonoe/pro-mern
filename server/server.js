ab52d736b0f2f5b4465d640b613c330ca570861a
deonoe <56742340+deonoe@users.noreply.github.com>
2020-08-20 18:24:48 +0200

    Input Validations

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer, UserInputError } = require("apollo-server-express");
 const { GraphQLScalarType } = require("graphql");
 const { Kind } = require("graphql/language");
 
@@ -12,11 +12,15 @@ const GraphQLDate = new GraphQLScalarType({
   },
 
   parseValue(value) {
-    return new Date(value);
+    const dateValue = new Date(value);
+    return isNaN(dateValue) ? undefined : dateValue;
   },
 
   parseLiteral(ast) {
-    return ast.kind === Kind.STRING ? new Date(ast.value) : undefined;
+    if (ast.kind === Kind.STRING) {
+      const value = new Date(ast.value);
+      return isNaN(value) ? undefined : value;
+    }
   },
 });
 
@@ -60,9 +64,9 @@ function setAboutMessage(_, { message }) {
 }
 
 function issueAdd(_, { issue }) {
+  validateIssue(issue);
   issue.created = new Date();
   issue.id = issuesDB.length + 1;
-  if (issue.status == undefined) issue.status = "New";
   issuesDB.push(issue);
   return issue;
 }
@@ -71,9 +75,29 @@ function issueList() {
   return issuesDB;
 }
 
+function validateIssue(_, { issue }) {
+  const errors = [];
+
+  if (issue.title.length < 3) {
+    errors.push("Field 'title' must be at least 3 characters long.");
+  }
+
+  if (issue.status == "Assigned" && !issue.owner) {
+    errors.push("Field 'owner' is required when status is 'Assigned'");
+  }
+
+  if (errors.length > 0) {
+    throw new UserInputError("Invalid input(s)", { erross });
+  }
+}
+
 const server = new ApolloServer({
   typeDefs: fs.readFileSync("./server/schema.graphql", "utf-8"),
   resolvers,
+  formatError: (error) => {
+    console.log(error);
+    return error;
+  },
 });
 
 const app = express();
