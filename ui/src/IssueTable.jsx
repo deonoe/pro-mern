080148ac21508c6d5f4831cd5d6cef311e0b7e1b
deonoe <56742340+deonoe@users.noreply.github.com>
2020-08-23 23:32:20 +0200

    Updating a Field

--- ui/src/IssueTable.jsx
+++ ui/src/IssueTable.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { Link, NavLink, withRouter } from "react-router-dom";
 
-const IssueRow = withRouter(({ issue, location: { search } }) => {
-  const selectLocation = { pathname: `/issues/${issue.id}`, search };
-  return (
-    <tr>
-      <td>{issue.id}</td>
-      <td>{issue.status}</td>
-      <td>{issue.owner}</td>
-      <td>{issue.created.toDateString()}</td>
-      <td>{issue.effort}</td>
-      <td>{issue.due ? issue.due.toDateString() : ""}</td>
-      <td>{issue.title}</td>
-      <td>
-        <Link to={`/edit/${issue.id}`}>Edit</Link>
-        {" | "}
-        <NavLink to={selectLocation}>Select</NavLink>
-      </td>
-    </tr>
-  );
-});
+const IssueRow = withRouter(
+  ({ issue, location: { search }, closeIssue, index }) => {
+    const selectLocation = { pathname: `/issues/${issue.id}`, search };
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.due ? issue.due.toDateString() : ""}</td>
+        <td>{issue.title}</td>
+        <td>
+          <Link to={`/edit/${issue.id}`}>Edit</Link>
+          {" | "}
+          <NavLink to={selectLocation}>Select</NavLink>{" "}
+          <button
+            type="button"
+            onClick={() => {
+              closeIssue(index);
+            }}
+          >
+            Close
+          </button>
+        </td>
+      </tr>
+    );
+  }
+);
 
 export default function IssueTable({ issues }) {
   const issueRows = issues.map((issue) => (
