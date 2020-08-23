62a7f06d582a7db49318a14c62b90d8062dcb61f
deonoe <56742340+deonoe@users.noreply.github.com>
2020-08-23 18:51:46 +0200

    Nested Routes

--- ui/src/IssueTable.jsx
+++ ui/src/IssueTable.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink, withRouter } from "react-router-dom";
 
-function IssueRow({ issue }) {
+const IssueRow = withRouter(({ issue, location: { search } }) => {
+  const selectLocation = { pathname: `/issues/${issue.id}`, search };
   return (
     <tr>
       <td>{issue.id}</td>
@@ -13,10 +14,12 @@ function IssueRow({ issue }) {
       <td>{issue.title}</td>
       <td>
         <Link to={`/edit/${issue.id}`}>Edit</Link>
+        {" | "}
+        <NavLink to={selectLocation}>Select</NavLink>
       </td>
     </tr>
   );
-}
+});
 
 export default function IssueTable({ issues }) {
   const issueRows = issues.map((issue) => (
